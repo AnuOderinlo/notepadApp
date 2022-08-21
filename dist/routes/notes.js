25d4946cbd87d32b28ef8ffe456323330d81f08e
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-08-21 23:26:30 +0100

    Edited the frontend and typos

--- dist/routes/notes.js
+++ dist/routes/notes.js
@@ -7,14 +7,6 @@ const express_1 = __importDefault(require("express"));
 const noteController_1 = require("../controllers/noteController");
 const authenticate_1 = require("../middleware/authenticate");
 const router = express_1.default.Router();
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-    // res.send("respond with a resource");
-    res.status(200).json({
-        status: "Success",
-        Message: "Successfully created a route",
-    });
-});
 // router.get("/notes/:id", getNote);
 // router.get("/notes", getAllNotes);
 // router.post("/notes", auth, createNote);
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -9,15 +9,6 @@ import {
 import { auth } from "../middleware/authenticate";
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  // res.send("respond with a resource");
-  res.status(200).json({
-    status: "Success",
-    Message: "Successfully created a route",
-  });
-});
-
 // router.get("/notes/:id", getNote);
 // router.get("/notes", getAllNotes);
 // router.post("/notes", auth, createNote);
